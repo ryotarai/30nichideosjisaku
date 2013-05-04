0c60222cb2a2a4b1684667853d340ff1d433216b
Ryota Arai <ryota.arai@gmail.com>
2013-05-04 18:14:10 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -17,16 +17,16 @@ elif [ ! -d $ZTO ]; then
     exit 1
 fi
 
-if [ ! -e $HOS/Q.app ]; then
-    echo "Create symlink to Q.app"
-    ln -s /Applications/Q.app ./Q.app
-fi
 
 # set z_tools
 mkdir -p $ZTO/qemu
 cp $TOL/z_tools/qemu/bios.bin $ZTO/qemu/bios.bin
 cp $TOL/z_tools/qemu/vgabios.bin $ZTO/qemu/vgabios.bin
-mv -f $HOS/Q.app $ZTO/qemu
+
+if [ ! -e $HOS/Q.app ]; then
+    echo "Create symlink to Q.app"
+    ln -s /Applications/Q.app $ZTO/qemu/Q.app
+fi
 
 # set projects
 cd $PRO
@@ -35,18 +35,12 @@ cd ../
 
 # set Makefile
 curl -L https://github.com/ryotarai/30nichideosjisaku/tarball/master | tar xz -C ./
-readonly SANDAI=$(find . -maxdepth 1 -regex ".*sandai-30nichideosjisaku.*" -type d)
+readonly SANDAI=$(find . -maxdepth 1 -regex "ryotarai-30nichideosjisaku.*" -type d)
 cd $SANDAI
 find . -regex ".*_day" -o -name "z_tools" -maxdepth 1 -type d | xargs -J % cp -rpf % ../
 cd ../
 
-# move file
-mkdir -p trash
-mv -f $PRO ./trash
-mv -f $TOL ./trash
-mv -f $SANDAI ./trash
-cd trash
-echo "このディレクトリは開発に必要の無いファイル郡です。削除しても構いません。" > readme.txt
+echo "You can remove $PRO, $TOL and $SANDAI"
 
 # All done!
 echo "おわり！がんばってOS作ってね！"
